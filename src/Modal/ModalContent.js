6d7db9106df812f37ff62274f16439a3fb195cff
Pascal Vos <vospascal@gmail.com>
2019-04-11 13:38:28 +0200

    styling chore

--- src/Modal/ModalContent.js
+++ src/Modal/ModalContent.js
@@ -18,7 +18,7 @@ const ButtonBottomContainer = styled("div")`
 `;
 
 const Button = styled("button")`
-  position: absolute;
+  position: relative;
   top: 0;
   right: 0;
   padding: 0.5em;
@@ -95,8 +95,12 @@ const HiddenText = styled("div")`
   width: 1px !important;
   white-space: nowrap !important;
 `;
+const HeaderButton = styled("div")`
+  position: sticky;
+`;
 
 const ModalCover = styled("div")`
+  box-sizing: border-box;
   position: fixed;
   top: 0;
   left: 0;
@@ -126,18 +130,19 @@ const ModalContent = ({
           ${modalContentStyle}
         `}
       />
-      <Modal
-        className={isOpen ? "modal-open" : "modal-closed"}
-        ref={modalRef}
-        width={optional.width}
-        data-role={role}
-      >
-        <Button aria-labelledby="close-modal" onClick={onClose} ref={buttonRef}>
-          <HiddenText id="close-modal" className="u-hide-visually">
-            Close Modal
-          </HiddenText>
-          x
-        </Button>
+      <Modal ref={modalRef} width={optional.width} data-role={role}>
+        <HeaderButton>
+          <Button
+            aria-labelledby="close-modal"
+            onClick={onClose}
+            ref={buttonRef}
+          >
+            <HiddenText id="close-modal" className="u-hide-visually">
+              Close Modal
+            </HiddenText>
+            x
+          </Button>
+        </HeaderButton>
         <ModalBody>
           {content}
           <ButtonBottomContainer>
